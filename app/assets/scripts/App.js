1f67c2dd668d96411c153c11adf0f730ea895ea0
Ibrahim <iteksoylu1@sheffield.ac.uk>
2021-07-16 05:03:31 +0300

    There is an error in the index.html still

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,5 @@
-alert("No need to run dev everytime we have watch:True")
+import '../styles/styles.css'
+//alert("No need to run dev everytime we have watch:True")
+if (module.hot) {
+    module.hot.accept()
+}
