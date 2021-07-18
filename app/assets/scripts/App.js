50c1a95d2bd992e37adb79601b2ef7c3523a96d4
Ibrahim <iteksoylu1@sheffield.ac.uk>
2021-07-18 20:01:40 +0300

    Side nav is done for all views

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
 //alert("No need to run dev everytime we have watch:True")
+
+let mobileMenu = new MobileMenu();
 if (module.hot) {
     module.hot.accept()
 }
