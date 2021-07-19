c8aaf6c4fa2387fa3c761d9f257734312fb1313b
Ibrahim <iteksoylu1@sheffield.ac.uk>
2021-07-19 05:07:40 +0300

    Nav bar customised now the links are working

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 //alert("No need to run dev everytime we have watch:True")
 
+let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
